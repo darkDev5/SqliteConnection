1fc92eed00ae1f3a22afdd3ac5722fa6ac6b5b18
Mehdi Lavasani <124900727+darkDev5@users.noreply.github.com>
2023-03-08 13:19:20 +0330

    >_0

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.darkdev5.lib.sqlite","c":"SqliteConnection","l":"connect(boolean)"},{"p":"org.darkdev5.lib.sqlite","c":"SqliteConnection","l":"SqliteConnection(String)","u":"%3Cinit%3E(java.lang.String)"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.darkdev5.lib.sqlite","l":"SqliteConnection"}];updateSearchResults();
